a6951a7bbb20bb449a0933d68ac8ab40fa673b76
Paul Ramnora <pramnora@yahoo.com>
2025-07-09 10:17:38 +0100

    Create touch-rm-01.sh
    Created file: [touch-rm-01.sh] Shows how to use both the 'touch'/and, 'rm' commands...which are used to either create/remove (delete) files...; and, a few different variations of these commands.

---
+++ filing/touch/touch-rm-01.sh
@@ -0,0 +1,39 @@
+# ---------------
+# Create files...
+# ---------------
+
+# Create a single file..
+
+touch file1.txt
+
+# create multiple files/(example 1/by manually naming them)
+
+touch file2.txt file3.txt file4.txt
+
+# create multiple files/(example 2/automatically, by using a loop)
+
+touch file{5..9}.txt
+
+# list files
+
+ls 
+
+# ---------------
+# remove files...
+# ---------------
+
+# remove a single file
+
+rm file1.txt
+
+# remove multiple files/(example 1/by manually naming them)
+
+rm file2.txt file3.txt file4.txt
+
+# remove multiple files/(example 2/automatically, by using a loop)
+
+rm file{5..9}.txt
+
+# list files
+
+ls
